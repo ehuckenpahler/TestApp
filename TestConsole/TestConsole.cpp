b0248f954d20902fb74a2f27707b3726afdccd73
Eric Huckenpahler <eric.huckenpahler@gmail.com>
2017-03-18 16:21:38 -0600

    Hello world added

---
+++ TestConsole/TestConsole.cpp
@@ -0,0 +1,13 @@
+// TestConsole.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    printf( "Hello World");
+    getch();
+    return 0;
+}
+
